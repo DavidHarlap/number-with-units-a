11dfd7af440192ed5e9238aa0236ad5656a8e6b6
DavidHarlap <adavid12@gmail.com>
2021-04-21 19:08:42 +0300

    add test

--- Test.cpp
+++ Test.cpp
@@ -32,69 +32,55 @@ TEST_CASE("operator -"){
     CHECK_EQ(a1-b1, NumberWithUnits(2.0035 ,"km"));
     CHECK_EQ(b1+a1, NumberWithUnits(2003.5 ,"m"));
     CHECK_THROWS(a1 + c1);
+}
 
+TEST_CASE("UNARY +"){
     // - onary
+    NumberWithUnits a1{2, "km"};
+    NumberWithUnits a2{3, "km"};
+    NumberWithUnits b1{3.5, "m"};
+    NumberWithUnits c1{3, "kg"};
+    NumberWithUnits b2{-3.5, "m"};
+
     CHECK_EQ(+b2, b1); //
     CHECK_EQ(+a1, a1); //
-        CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
-    CHECK_EQ(+a1, a1); //
+}
 
+TEST_CASE("UNARY -"){
+    // - onary
+     NumberWithUnits a1{2, "km"};
+    NumberWithUnits a2{3, "km"};
+    NumberWithUnits b1{3.5, "m"};
+    NumberWithUnits c1{3, "kg"};
+    NumberWithUnits b2{-3.5, "m"};
 
+    CHECK_EQ(b2, -b1); //
 }
     
+TEST_CASE("operator *"){
+    NumberWithUnits a1{2, "km"};
+    NumberWithUnits a2{3, "km"};
+    NumberWithUnits b1{3.5, "m"};
+    NumberWithUnits c1{3, "kg"};
+    NumberWithUnits b2{-3.5, "m"};
+
+    CHECK_EQ(3*a1, NumberWithUnits(6,"km"));
+    CHECK_EQ(a1*3, NumberWithUnits(6,"km"));
+    CHECK_EQ(b2*0, NumberWithUnits(0 ,"m"));
+    CHECK_EQ(b1*-2, NumberWithUnits(-7 ,"m"));
+    }
 
 
+TEST_CASE("throw"){
+    NumberWithUnits a1{2, "km"};
+    NumberWithUnits a2{3, "km"};
+    NumberWithUnits b1{3.5, "m"};
+    NumberWithUnits c1{3, "kg"};
+    NumberWithUnits b2{-3.5, "m"};
 
+    CHECK_NOTHROW(a1++);
+    CHECK_THROWS(a1++*3);
+    CHECK_NOTHROW(a1++);
+    CHECK_NOTHROW(a1++);
 
-
-
-    NumberWithUnits u1{500, "cm"};
-    NumberWithUnits u2{1000, "cm"}; //10 meter
-    NumberWithUnits u3{50, "m"};
-    NumberWithUnits u4{5000, "m"}; //5 km
-    NumberWithUnits u5{2, "km"};
-    NumberWithUnits u6{2, "km"};
-    NumberWithUnits u7{200, "g"};
-    NumberWithUnits u8{1000, "g"}; //1 kg
-    NumberWithUnits u9{500, "kg"};
-    NumberWithUnits u10{1200, "kg"}; //1.2 ton
-    NumberWithUnits u11{1.5, "ton"};
-    NumberWithUnits u12{2, "ton"};
-    NumberWithUnits u13{30, "sec"};
-    NumberWithUnits u14{60, "sec"}; //1 min
-    NumberWithUnits u16{30, "min"};
-    NumberWithUnits u17{0.5, "hour"};
-    NumberWithUnits u18{60, "hour"};
+}
